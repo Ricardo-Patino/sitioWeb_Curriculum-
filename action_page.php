e7891a9bd192dd1c60a0b1468fe70184b393701b
Rick <77856968+Ricardo-Patino@users.noreply.github.com>
2022-09-12 18:31:52 -0600

    action_page
    paguiba en php para enviar al servidor y HTML para dar mensaje de gracias.

---
+++ action_page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Su información fué enviada pronto nos pondremos en contaACTO CONUSTED</title>
+        <meta charset='UTF-8'>
+        <link rel="stylesheet" href="C:\Users\Racha\OneDrive - Universidad Fidélitas\Documentos\Fidelitas - Ing Sistemas\Cert en Diseño y Desarrollo Web\HTML5 y CSS3\Sem 4 Páguinas Responsivas\Evaluación\estilos.CSS">
+    </head>
+    <body>
+        <header>
+            <h1>Su información fur recibida.</h1>
+            <nav style="background: #320d5538">
+                <a href="index.html">Inicio</a>
+                <a href="habilidadesTecnicas.html">Habilidades Técnicas</a>
+                <a href="experienciaLaboral.html">Experiencia Laboral</a>
+                <a href="Proyectos Personales - EducaciónCertis.html">Proyectos Personales - Educación/Certis</a>
+                <a href="referencias.html">Referencias</a>
+                <a href="contacto.html">Contacto</a>
+            </nav>
+            </nav>
+        </header>
+        <article>
+            <br>
+            <br>
+            <img class='foto' src='https://previintegral.weebly.com/uploads/2/0/1/8/20183403/2146383.jpg?1368407660'>
+            <section>
+                <a class='nombre'>¡Gracias! ¡Su información fue enviada con correctamente. Nos prondemos en contacto en menos de 48 horas!</a>
+                <br>
+            </section>
+            <div class='limpiar'></div>
+        </article>
+        <footer>
+            <p>Tarea de semana 4 - Creada por Ricardo Patiño</p>
+        </footer>
+        <div id='informacion'>Tarea de semana 4 - Ricardo Patiño - curso HTML y CSS</div>
+    </body>
+</html>
